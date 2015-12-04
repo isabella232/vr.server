62ee025a7200a2418098060d00b03e840a40af07
Jason R. Coombs <jaraco@jaraco.com>
2015-12-04 11:04:35 -0500

    Avoid error when database doesn't exist

--- vr/server/tests/dbsetup.sql
+++ vr/server/tests/dbsetup.sql
@@ -1,5 +1,5 @@
 create role raptor with superuser;
 alter role raptor with login;
 alter role raptor with password 'MZpuvFzZ';
-drop database velotest;
+drop database if exists velotest;
 create database velotest;
