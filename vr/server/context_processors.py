274d18c075c65b36bfdd93c0b8731e71cbb4a87b
Lorenzo Bolla <lorenzo.bolla@yougov.com>
2015-12-14 11:17:24 +0000

    Show server version in UI's footer

--- vr/server/context_processors.py
+++ vr/server/context_processors.py
@@ -3,6 +3,11 @@ from django.conf import settings
 from vr.server.models import Swarm, Squad
 
 
+def get_version(pkgname):
+    import pkg_resources
+    return pkg_resources.get_distribution(pkgname).version
+
+
 def raptor(request):
     return {
         # For showing a list of all swarms in the nav
@@ -13,4 +18,5 @@ def raptor(request):
         'site_title': getattr(settings, 'SITE_TITLE', 'Velociraptor'),
         'custom_css': getattr(settings, 'CUSTOM_CSS', None),
         'max_events': getattr(settings, 'EVENTS_BUFFER_LENGTH', 100),
+        'version': get_version('vr.server'),
     }
