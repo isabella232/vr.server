c88d60ce6bc458a1411205aa6d11d3cd8f3bf18b
Lorenzo Bolla <lorenzo.bolla@yougov.com>
2016-02-03 14:55:23 +0000

    Bump

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ pytest_runner = ['pytest_runner'] if needs_pytest else []
 setup(
     name='vr.server',
     namespace_packages=['vr'],
-    version='4.9.1',
+    version='4.9.2',
     author='Brent Tubbs',
     author_email='brent.tubbs@gmail.com',
     packages=find_packages(),
