a3aa300fda07d9dc3bdd0a674fcfe3c14290c216
Lorenzo Bolla <lorenzo.bolla@yougov.com>
2016-08-19 14:20:15 +0100

    Bump version to 5.3.1

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ pytest_runner = ['pytest_runner'] if needs_pytest else []
 setup(
     name='vr.server',
     namespace_packages=['vr'],
-    version='5.2.2',
+    version='5.3.1',
     author='Brent Tubbs',
     author_email='brent.tubbs@gmail.com',
     packages=find_packages(),
