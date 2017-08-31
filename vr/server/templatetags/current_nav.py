b99b74a4a2459a8a2e21f104f6c215a86f132a91
Jason R. Coombs <jason.coombs@yougov.com>
2017-08-31 11:03:46 -0400

    Fix syntax error on Python 3

--- vr/server/templatetags/current_nav.py
+++ vr/server/templatetags/current_nav.py
@@ -2,13 +2,16 @@ from django import template
 
 register = template.Library()
 
+
 @register.tag
 def current_nav(parser, token):
     args = token.split_contents()
     template_tag = args[0]
     if len(args) < 2:
-        raise template.TemplateSyntaxError, "%r tag requires at least one argument" % template_tag
+        msg = "%r tag requires at least one argument" % template_tag
+        raise template.TemplateSyntaxError(msg)
     return NavSelectedNode(args[1])
+
 
 class NavSelectedNode(template.Node):
     def __init__(self, url):
@@ -17,7 +20,7 @@ class NavSelectedNode(template.Node):
     def render(self, context):
         path = context['request'].path
         pValue = template.Variable(self.url).resolve(context)
-        if (pValue == '/' or pValue == '') and not (path  == '/' or path == ''):
+        if (pValue == '/' or pValue == '') and not (path == '/' or path == ''):
             return ""
         if path.startswith(pValue):
             return 'active'
